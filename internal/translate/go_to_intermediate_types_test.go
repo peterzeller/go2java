fa6d446bde63918c607173b5b3c5144d92996142
Peter Zeller <p_zeller@cs.uni-kl.de>
2026-05-11 18:26:13 +0200

    Add looped unsigned operator combo coverage tests

--- internal/translate/go_to_intermediate_types_test.go
+++ internal/translate/go_to_intermediate_types_test.go
@@ -1,8 +1,14 @@
 package translate
 
 import (
+	"fmt"
 	"go/parser"
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
+
+	"go2java/internal/syntaxtree"
 )
 
 func TestLowerType_AllGoIntegerTypes(t *testing.T) {
@@ -56,5 +62,62 @@ func TestBoxedType_UnsignedUsesJavaBoxedPrimitives(t *testing.T) {
 		if got := boxedType(typ); got != want {
 			t.Fatalf("boxedType(%s) = %q, want %q", in, got, want)
 		}
+	}
+}
+
+func TestUnsignedOpsUseJavaHelpers_AllValueCombinations(t *testing.T) {
+	ops := []string{"<", "<=", ">", ">=", "==", "!=", "/", "%"}
+	values32 := []string{"1", "2", "0x80000000"}
+	values64 := []string{"1", "3", "0x8000000000000000"}
+
+	for _, op := range ops {
+		for _, a := range values32 {
+			for _, b := range values32 {
+				if (op == "/" || op == "%") && b == "0" {
+					continue
+				}
+				assertUnsignedHelperForCombo(t, "uint32", "Integer", op, a, b)
+			}
+		}
+		for _, a := range values64 {
+			for _, b := range values64 {
+				if (op == "/" || op == "%") && b == "0" {
+					continue
+				}
+				assertUnsignedHelperForCombo(t, "uint64", "Long", op, a, b)
+			}
+		}
+	}
+}
+
+func assertUnsignedHelperForCombo(t *testing.T, goType, helper, op, a, b string) {
+	t.Helper()
+	dir := t.TempDir()
+	src := fmt.Sprintf("package main\nimport \"fmt\"\nfunc f(a %s, b %s){fmt.Println(a %s b)}\nfunc main(){f(%s,%s)}\n", goType, goType, op, a, b)
+	path := filepath.Join(dir, "main.go")
+	if err := os.WriteFile(path, []byte(src), 0o600); err != nil {
+		t.Fatal(err)
+	}
+	tree, err := syntaxtree.LoadTypedFile(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ir, err := GoToIntermediate(tree)
+	if err != nil {
+		t.Fatal(err)
+	}
+	javaCode, err := IntermediateToJava(ir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := helper + ".compareUnsigned"
+	if op == "/" {
+		want = helper + ".divideUnsigned"
+	}
+	if op == "%" {
+		want = helper + ".remainderUnsigned"
+	}
+	if !strings.Contains(javaCode, want) {
+		t.Fatalf("%s %s %s expected %s in generated Java:\n%s", goType, a, op+" "+b, want, javaCode)
 	}
 }
